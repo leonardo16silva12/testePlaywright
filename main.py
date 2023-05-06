c3c6afa580135b814d065f60fb8036acc7343643
Leonardo Silva <yleo545@hotmail.com>
2023-05-06 20:48:19 -0300

    X4
    Removendo F11

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ with sync_playwright() as p:
     page = navegador.new_page()
     page.set_viewport_size({"width": 1280, "height": 720})
     page.goto("http://192.168.0.53:8080/troppus/vue/tab/tab658/form.do")
-    page.keyboard.press('F11')
 
     # LOGIN
     t1 = page.locator("input[name=id]")
